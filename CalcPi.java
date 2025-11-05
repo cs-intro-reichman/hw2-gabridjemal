2c3d478ac6e1a7a44e10328fe4d363e851e84541
Gabriel Djemal <gabrieljoseph.djemal@post.runi.ac.ilgit config --list>
2025-11-05 15:21:05 +0200

    Completed Assignment

--- CalcPi.java
+++ CalcPi.java
@@ -7,21 +7,22 @@ public class CalcPi {
 			int denominator1 = 1;
 			int denominator2 = 3;
 			int i = 0;
-			while (i<(n/2+1)) {
-				double plus = (double) 1/denominator1;
-				double minus = (double) 1/denominator2;
+			while (i<(n/2)) {
+				double plus =  1.0/denominator1;
+				double minus = 1.0/denominator2;
 				denominator1 = denominator1 + 4;
 				denominator2 = denominator2 + 4;
 				piOver4 = piOver4+plus-minus;
 				i=i+1;
-			}}
+			}
+			piOver4 = piOver4 + 1.0 / denominator1;}
 		else
 			{int denominator1 = 1;
 			int denominator2 = 3;
 			int i = 0;
 			while (i<(n/2)) {
-				double plus = (double) 1/denominator1;
-				double minus = (double) 1/denominator2;
+				double plus = 1.0/denominator1;
+				double minus = 1.0/denominator2;
 				denominator1 = denominator1 + 4;
 				denominator2 = denominator2 + 4;
 				piOver4 = piOver4+plus-minus;
